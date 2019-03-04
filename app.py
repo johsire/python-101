3d529ba6dcd9a1013bb14b20c06fb5e38b8d9da3
dev_johsire <josiah5115@gmail.com>
2019-03-03 23:56:37 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,98 @@
+
+# OUTPUT, LOG, PRINT
+
+# print('Coder Joh')
+# print('o----')
+# print(' ||||')
+
+# expression: a piece of code that produces a value;
+# print('*' * 10)
+
+# VARIABLES - temp store data in a computer memory location
+# # integer
+# price = 10
+# price = 20
+# # float
+# rating = 4.9
+# print(price)
+# # strings
+# name = 'Joh'
+# # boolean
+# is_public = True
+
+# name = input('What is your name? ')
+# print('Hi ' + name)
+
+# color = input('What color do you like? ')
+# print(name + ' likes ' + color)
+
+# TYPE CONVERSION
+# birth_year = input('Birth year')
+# age = 2019 - int(birth_year)
+# print(age)
+
+# int()
+# float()
+# bool()
+
+# weight_lbs = input('Weight in pounds? ')
+# convert_weight = int(weight_lbs) / 2.2
+# print(convert_weight)
+
+# STRINGS
+# A series of characters.
+# Can use 'single' or "double" quotes to define strings.
+# To write multi line sentences we use three quotation marks to indicate this.
+
+# print('''
+# Hello John,
+#
+# I hope you have a wonderful day!
+#
+# Thanks,
+# The Support Team
+# Jessy Pinkman
+# ''')
+
+
+# FORMATTED STRINGS
+
+# Formatted String are easier to visualize the end result than Concatenated Strings in some cases.
+# Formatted Strings contain holes/place holders that are symbolized by {} braces and we start the sentence with an 'f';
+
+# first = 'Stewie'
+# # last = 'Griffin'
+# # message = first + ' [' + last + '] is an evil baby genius'
+# # msg = f'{first} [{last}] is an evil baby genius'
+# # print(message)
+# # print(msg)
+
+# STING METHODS
+# Strings come with their own in-built functions - methods;
+# course = 'Python for Beginners'
+# print(course)
+
+
+# ARITHMETIC OPERATORS
+
+# Operators precedence/ order of computing
+
+# BODMAS
+
+# Brackets
+# Exponentiation/ to the power of
+# Division or Multiplication
+# Addition or Subtraction
+
+
+# MATH FUNCTIONS
+
+import math
+
+# you have to import the math module in order to get the in-built functions that come with it - methods
+# To access the methods we start with math(dot). followed by the name of the method you want to use
+# absolute method always returns a positive number
+# abs()
+# math.floor()
+# math.ceil()    e.t.c
+
